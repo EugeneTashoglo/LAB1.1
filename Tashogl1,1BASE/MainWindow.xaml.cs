a5b036390f3ab3b62ba06572781f8b32db957e03
Евгений Ташогло <tashoglo00@mail.ru>
2022-02-01 23:53:10 +0200

    Добавьте файлы проекта.

---
+++ Tashogl1,1BASE/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tashogl1_1BASE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(Hour.Text);
+            int y = int.Parse(Tarif.Text);
+            int a = x * y;
+            end.Text = $"общая стоимость работы {a}";
+        }
+    }
+}
